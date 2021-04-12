6dbed9bd809798530a2e8dfb1351d50380c0f801
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-04-12 15:08:56 -0300

    Update movie.js

--- movie.js
+++ movie.js
@@ -2,7 +2,7 @@ var main = document.querySelectorAll('.main .center')[0];
 
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&languague=en-US&page=1')
 .then(response=> response.json())
-.then(data=>{
+ .then(data=>{
     data.results.map((i,j)=>{
 
         if(j > 10)
