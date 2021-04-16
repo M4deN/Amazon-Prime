9abdbcad22612270e6c2973a618c683b653cf332
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-04-16 18:49:04 -0300

    Update movie.js

--- movie.js
+++ movie.js
@@ -94,3 +94,34 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
 
     })
 })
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&languague=en-US&page=4')
+.then(response=> response.json())
+ .then(data=>{
+    data.results.map((i,j)=>{
+        
+        if(j > 18)
+            return;
+        if(j == 0){
+
+            main.innerHTML+=`
+                
+                 <div class="banner-principal" style="margin-top:20px;background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
+            `;
+        }else{
+            if(j==1){
+                main.innerHTML+=`
+                <h2>Ação</h2>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                `;
+            }else{
+
+                main.innerHTML+=`
+                    
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                `;
+            }
+        
+        }
+
+    })
+})
