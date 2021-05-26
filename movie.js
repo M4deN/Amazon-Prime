ec90c6aac6b3eab4caa78c64bee3014ac250042a
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-05-26 10:12:35 -0300

    Update movie.js

--- movie.js
+++ movie.js
@@ -5,7 +5,7 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
  .then(data=>{
     data.results.map((i,j)=>{
         
-        if(j > 18)
+        if(j > 16)
             return;
         if(j == 0){
 
@@ -17,13 +17,13 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
             if(j==1){
                 main.innerHTML+=`
                     <h2>Lançamentos</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }else{
 
                 main.innerHTML+=`
                     
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }
         
@@ -37,25 +37,25 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
  .then(data=>{
     data.results.map((i,j)=>{
         
-        if(j > 18)
+        if(j > 16)
             return;
         if(j == 0){
 
             main.innerHTML+=`
-                <h2>Assista Aos Melhores Filmes</h2>
-                 <div class="card-movie" style="background-image:url('https://image.tmdb.org/t/p/w200`+i.backdrop_path+`')"></div>
+
+                 <div class="banner-principal" style="background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
             `;
         }else{
             if(j==1){
                 main.innerHTML+=`
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <h2>Lançamentos</h2>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }else{
 
                 main.innerHTML+=`
                     
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }
         
@@ -68,7 +68,7 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
  .then(data=>{
     data.results.map((i,j)=>{
         
-        if(j > 18)
+        if(j > 16)
             return;
         if(j == 0){
 
@@ -80,13 +80,13 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
             if(j==1){
                 main.innerHTML+=`
                 <h2>Para Assistir Agora</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }else{
 
                 main.innerHTML+=`
                     
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }
         
@@ -99,7 +99,7 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
  .then(data=>{
     data.results.map((i,j)=>{
         
-        if(j > 18)
+        if(j > 16)
             return;
         if(j == 0){
 
@@ -111,13 +111,13 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
             if(j==1){
                 main.innerHTML+=`
                 <h2>Ação</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }else{
 
                 main.innerHTML+=`
                     
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }
         
@@ -130,7 +130,7 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
  .then(data=>{
     data.results.map((i,j)=>{
         
-        if(j > 18)
+        if(j > 16)
             return;
         if(j == 0){
 
@@ -142,13 +142,13 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05
             if(j==1){
                 main.innerHTML+=`
                 <h2>Suspense</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }else{
 
                 main.innerHTML+=`
                     
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w300`+i.poster_path+`" /></div>
                 `;
             }
         
