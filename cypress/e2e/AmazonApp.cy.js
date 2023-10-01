41552ba795046906c38568f547b8706ecf7ebc30
M4deN <alexdesaran@outlook.com>
2023-10-01 13:52:29 -0300

    Add Feedback visual API Testing

--- cypress/e2e/AmazonApp.cy.js
+++ cypress/e2e/AmazonApp.cy.js
@@ -9,12 +9,12 @@ describe('Navegação no Menu', () => {
   })
 
   it('Deve navegar para a página TV Shows', () => {   
-      cy.get('.menu-itens a[href="#"]').eq(0).click() // o índice pode variar dependendo da ordem dos links
+      cy.get('.menu-itens a[href="#"]').eq(0).click() 
       cy.url().should('include', '#')
   })
 
   it('Deve navegar para a página Movies', () => {      
-      cy.get('.menu-itens a[href="#"]').eq(1).click() // o índice pode variar dependendo da ordem dos links
+      cy.get('.menu-itens a[href="#"]').eq(1).click() /
       cy.url().should('include', '#')
   })
   it('Deve navegar para a página Login', () => {     
@@ -25,36 +25,37 @@ describe('Navegação no Menu', () => {
         cy.get('.input-search input').should('be.visible').type('Nome do filme ou série')
         cy.get('.input-search i').should('be.visible').click()      
   })
-  it('Deve abrir o link do Facebook em uma nova guia', () => {      
-      cy.get('#rodape a[href="https://www.facebook.com/alex.leandro.0007"]').should('have.attr', 'target', '_blank')
-      cy.get('#rodape a[href="https://www.instagram.com/alexdesaran/"]').should('have.attr', 'target', '_blank')
-  })
+  it('Deve abrir o link do Facebook e Instagram em uma nova guia', () => {      
+    cy.get('footer a[href="https://www.facebook.com/Madenxx/"]').should('have.attr', 'target', '_blank')
+    cy.get('footer a[href="https://www.instagram.com/madenx/"]').should('have.attr', 'target', '_blank')
+})
+
     it('Deve exibir o logo corretamente', () => {
         cy.get('.logo img').should('have.attr', 'src', '../Amazon-Prime/Imagens/Amazon_Logo.png')
     })
 
     it('Deve permitir a navegação no menu', () => {
-        
         cy.contains('a', 'Home').click()
         cy.url().should('eq', 'https://m4den.github.io/Amazon-Prime/home.html')
-        cy.contains('a', 'TV Shows').should('be.visible').click()        
-        cy.contains('a', 'Movies').should('be.visible').click()        
-        cy.contains('a', 'Cadastre-se').should('be.visible').click()       
-        cy.contains('a', 'Cadastrar').should('be.visible').click()
+        
+        cy.contains('a', 'TV Shows').should('be.visible').click()
+        cy.url().should('eq', 'https://m4den.github.io/Amazon-Prime/home.html#') 
     })
+    
 
     it('Deve permitir a realização de buscas', () => {        
         cy.get('.input-search input').should('be.visible').type('Termo de busca')
         cy.get('.input-search i').should('have.class', 'fas fa-search')
     });
 
-    it('Deve exibir o rodapé corretamente', () => {        
-        cy.contains('Copyright © 2021 - by Alécio Leandro De Medeiros')
-        cy.get('#rodape a').should('have.length', 2)
-        cy.get('#rodape a').first().should('have.attr', 'href', 'https://www.facebook.com/alex.leandro.0007')
-        cy.get('#rodape a').last().should('have.attr', 'href', 'https://www.instagram.com/alexdesaran/')
+    it('Deve exibir o footer corretamente', () => {        
+        cy.contains('Copyright © 2020 Desenvolvido por Alécio L. Medeiros').should('be.visible')
+        cy.get('footer a').should('have.length', 4)
+        cy.get('footer a').eq(0).should('have.attr', 'href', 'https://www.facebook.com/Madenxx/')
+        cy.get('footer a').eq(1).should('have.attr', 'href', 'https://www.linkedin.com/in/madenx')
+        cy.get('footer a').eq(2).should('have.attr', 'href', 'https://github.com/M4deN');
+        cy.get('footer a').eq(3).should('have.attr', 'href', 'https://www.instagram.com/madenx/')
     })
-
 
   context('Carregamento de Filmes API Externa', () => {
     it('Deve carregar filmes da API corretamente', () => {        
@@ -76,9 +77,10 @@ describe('Navegação no Menu', () => {
           }
         }).then((response) => {
           expect(response.status).to.eq(200);
-          expect(response.body.results).to.be.an('array').that.is.not.empty;
-        });
-      });
+          expect(response.body.results).to.be.an('array').that.is.not.empty
+          cy.wait(1000)
+        })
+      })
   })
 
   it('Deve obter os filmes populares', () => {
@@ -92,9 +94,10 @@ describe('Navegação no Menu', () => {
       }
     }).then((response) => {
       expect(response.status).to.eq(200);
-      expect(response.body.results).to.have.length.greaterThan(0);
-    });
-  });
+      expect(response.body.results).to.have.length.greaterThan(0)
+      cy.wait(1000)
+    })
+  })
 
   it('Deve obter os detalhes de um filme específico', () => {
     cy.request({
@@ -106,7 +109,8 @@ describe('Navegação no Menu', () => {
       }
     }).then((response) => {
       expect(response.status).to.eq(200);
-      expect(response.body.title).to.eq('Fight Club');
-    });
-  });
+      expect(response.body.title).to.eq('Fight Club')
+      cy.wait(1000)
+    })
+  })
 })
